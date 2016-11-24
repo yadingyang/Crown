c5f6738ea80ddd198ee01cb474e3a6efc172284d
yadingyang <yyang@moreheadstate.edu>
2016-11-23 20:53:03 -0800

    search

--- CM/Controllers/IndexController.cs
+++ CM/Controllers/IndexController.cs
@@ -12,36 +12,47 @@ namespace CM.Controllers
     public class IndexController : Controller
     {
         private CrownContext db = new CrownContext();
-        
-            public ActionResult Index(string sortOrder)
-            {
 
-            //var cate = from s in db.Cat
-            //                select s;
-
-            //return View(cate.ToList());
-
+        public ActionResult Index()
+        {
             return View();
 
         }
 
+        public ActionResult Search (string SearchString)
+        {
+            var result = from s in db.Pro
+                         select s;
+
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                var Result = result.Where(s => s.Name.Contains(SearchString));
+                
+                return View(Result);
+            }
+            
+            else
+                { return View(); }
+        }
+
+       
 
 
 
 
         public JsonResult GetCate()
-    {
-        var cate = from s in db.Cat
-                   select s;
-       
-        var result = cate.ToList();
-        return Json(result, JsonRequestBehavior.AllowGet);
+        {
+            var cate = from s in db.Cat
+                       select s;
 
-    }
-    
+            var result = cate.ToList();
+            return Json(result, JsonRequestBehavior.AllowGet);
+
         }
 
-    
+    }
+
+
 
 
 
